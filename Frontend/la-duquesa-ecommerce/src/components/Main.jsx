f5542783ae93adc3fdbb7aafe1feb6fe5f086cd0
SoyJeroMe <ojeronimo360@gmail.com>
2024-05-17 18:50:35 -0500

    avanzar muestra de productos en mobile

--- Frontend/la-duquesa-ecommerce/src/components/Main.jsx
+++ Frontend/la-duquesa-ecommerce/src/components/Main.jsx
@@ -18,23 +18,25 @@ function Main () {
           placeholder="Busca un producto..."
         />
       </div>
-      <section className="flex items-center justify-center">
-        <article className="m-4">
-          <img src={brioche} />
-          <p className='flex justify-center'>Brioche</p>
-        </article>
-        <article className="m-4 flex flex-col">
-          <img src={cinnabon} />
-          <p className='flex justify-center'>Cinnabon</p>
-        </article>
-        <article className="m-4">
-          <img src={galletas} />
-          <p className='flex justify-center'>Galletas</p>
-        </article>
-        <article className="m-4">
-          <img src={croissant} />
-          <p className='flex justify-center'>Croissant</p>
-        </article>
+      <section className="flex items-center justify-center p-2">
+        <div className="font-medium m-4 h-36 rounded-full bg-[#F3F0EC] p-2 hover:bg-[#BD6292] hover:text-slate-50">
+          <article>
+            <img className="hover:bg-white rounded-full" src={brioche} />
+            <p className="flex justify-center">Brioche</p>
+          </article>
+        </div>
+        <div className="font-medium m-4 h-36 rounded-full bg-[#F3F0EC] p-2 hover:bg-[#BD6292] hover:text-slate-50">
+          <img className="hover:bg-white rounded-full" src={cinnabon} />
+          <p className="flex justify-center">Cinnabon</p>
+        </div>
+        <div className="font-medium m-4 h-36 rounded-full bg-[#F3F0EC] p-2 hover:bg-[#BD6292] hover:text-slate-50">
+          <img className="hover:bg-white rounded-full" src={galletas} />
+          <p className="flex justify-center">Galletas</p>
+        </div>
+        <div className="font-medium m-4 h-36 rounded-full bg-[#F3F0EC] p-2 hover:bg-[#BD6292] hover:text-slate-50">
+          <img className="hover:bg-white rounded-full" src={croissant} />
+          <p className="flex justify-center">Croissant</p>
+        </div>
       </section>
     </section>
   )
