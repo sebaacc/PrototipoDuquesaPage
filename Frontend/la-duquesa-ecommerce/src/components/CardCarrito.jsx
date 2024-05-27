e3d78581ae6e1fc74cbe6b40e49de158472c6286
Sebastián-Markoja <sebaacc11@gmail.com>
2024-05-27 20:30:58 -0300

    carrito de compras y correcciones varias

--- Frontend/la-duquesa-ecommerce/src/components/CardCarrito.jsx
+++ Frontend/la-duquesa-ecommerce/src/components/CardCarrito.jsx
@@ -1,18 +1,54 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
-function CardCarrito ({ producto }) {
-  const { nombre, img, precio } = producto
+function CardCarrito ({ producto, setProductos }) {
+  const { nombre, img, precio, cantidad } = producto
+  // Define el estado para el valor seleccionado del select
+  const [selectedValue, setSelectedValue] = useState('opcion2') // Valor por defecto
+
+  // Maneja el cambio del valor seleccionado
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value)
+    const newQuantity = parseInt(event.target.value, 10)
+    setProductos((prevProductos) =>
+      prevProductos.map((p) =>
+        p.nombre === producto.nombre ? { ...p, cantidad: newQuantity } : p
+      )
+    )
+    console.log('valor seleccionado' + selectedValue)
+  }
 
   return (
-    <article className="flex mb-10 md:mr-6 lg:mr-8 ml-10 mt-8">
-      <img className="w-48 rounded-l-xl object-cover" src={img} alt={nombre} />
-      <span className="bg-[#D9D9D9] rounded-r-xl p-2 flex flex-col justify-center items-center">
-        <p className="font-bold mb-3">{nombre}</p>
-        <p className="mb-4">Cantidad</p>
-        <p className="mb-4">{precio}</p>
-        <p className="mb-4">Envío: Gratis</p>
-      </span>
-    </article>
+    <>
+      <div className="w-11/12 self-center flex mb-10 md:mr-6 md:justify-center lg:mr-8 mt-8 border-2 border-[#D1D1D1] rounded-xl">
+        <img
+          className="w-2/4 rounded-l-xl object-cover"
+          src={img}
+          alt={nombre}
+        />
+        <span className="w-2/4 bg-[#D9D9D9] rounded-r-xl p-2 flex flex-col justify-center items-center">
+          <p className="font-bold mb-3">{nombre}</p>
+          <div className=" flex gap-2">
+            <p className="mb-4">Cantidad:</p>
+
+            <select
+              className="py-2 px-1 border border-gray-200 mr-6 focus:outline-none"
+              value={producto.cantidad}
+              onChange={handleChange}
+            >
+              {[...Array(10).keys()].map((num) => (
+                <option key={num + 1} value={num + 1}>
+                  {num + 1}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <p className="mb-4 text-[#2D5651]">${precio * cantidad}</p>
+          <p className="mb-4  text-[#2D5651]">Envío: Gratis</p>
+        </span>
+      </div>
+    </>
   )
 }
 
@@ -21,8 +57,10 @@ CardCarrito.propTypes = {
   producto: PropTypes.shape({
     nombre: PropTypes.string.isRequired,
     img: PropTypes.string.isRequired,
-    precio: PropTypes.string.isRequired
-  }).isRequired
+    precio: PropTypes.string.isRequired,
+    cantidad: PropTypes.number.isRequired
+  }).isRequired,
+  setProductos: PropTypes.func.isRequired
 }
 
 export default CardCarrito
--- Frontend/la-duquesa-ecommerce/src/routes/CarritoDeCompras.jsx
+++ Frontend/la-duquesa-ecommerce/src/routes/CarritoDeCompras.jsx
@@ -1,16 +1,45 @@
+import { useState } from 'react'
 import CardCarrito from '../components/CardCarrito'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 function CarritoDeCompras () {
-  const tortaBrownie = {
-    nombre: 'Torta Brownie',
-    descripcion: 'alguna descripcion',
-    img: 'https://aprende.com/wp-content/uploads/2020/10/brownies-postre_opt-940x580.jpg',
-    precio: '62000'
-  }
+  const listaProductos = [
+    {
+      nombre: 'Torta Brownie',
+      descripcion: 'alguna descripcion',
+      img: 'https://aprende.com/wp-content/uploads/2020/10/brownies-postre_opt-940x580.jpg',
+      precio: '62000',
+      cantidad: 1
+    },
+    {
+      nombre: 'Croissant',
+      descripcion: 'alguna descripcion',
+      img: 'https://aprende.com/wp-content/uploads/2020/10/brownies-postre_opt-940x580.jpg',
+      precio: '3800',
+      cantidad: 3
+    },
+    {
+      nombre: 'Cupcake de Naranja con crocante',
+      descripcion: 'alguna descripcion',
+      img: 'https://aprende.com/wp-content/uploads/2020/10/brownies-postre_opt-940x580.jpg',
+      precio: '5000',
+      cantidad: 2
+    }
+  ]
+  const [productos, setProductos] = useState(listaProductos)
+
   return (
     <div>
       <Navbar />
-      <CardCarrito producto={tortaBrownie} />
+      <h1 className="text-4xl font-black pl-9 pr-9 sm:text-center">
+        Carrito de productos
+      </h1>
+      <div className="flex bg-[#F6F6F6] border-2 border-solid border-[#E9E9E9] rounded-xl flex-col justify-center items-end m-4 w-11/12">
+        {productos.map((producto, index) => (
+          <CardCarrito key={index} producto={producto} setProductos={setProductos} />
+        ))}
+      </div>
+      <Footer />
     </div>
   )
 }
