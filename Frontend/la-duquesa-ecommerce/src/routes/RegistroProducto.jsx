765f701b1cc6690a1732a650d55884e5e68fb2ef
SoyJeroMe <ojeronimo360@gmail.com>
2024-05-28 20:10:49 -0500

    agregar alerta de creacion de producto

--- Frontend/la-duquesa-ecommerce/src/routes/RegistroProducto.jsx
+++ Frontend/la-duquesa-ecommerce/src/routes/RegistroProducto.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Navbar from '../components/Navbar'
 
 function RegistroProducto () {
@@ -10,6 +10,7 @@ function RegistroProducto () {
   })
 
   const [errors, setErrors] = useState({})
+  const [showAlert, setShowAlert] = useState(false)
 
   const handleChange = (e) => {
     const { id, value } = e.target
@@ -36,9 +37,11 @@ function RegistroProducto () {
     setErrors(formErrors)
 
     if (Object.keys(formErrors).length === 0) {
-      // Form is valid, submit the data
+      setShowAlert(true)
+      setTimeout(() => {
+        setShowAlert(false)
+      }, 3000)
       console.log('Form data:', formData)
-      // Add form submission logic here (e.g., API call)
     }
   }
 
@@ -133,11 +136,31 @@ function RegistroProducto () {
         <div className="flex justify-center">
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-[#BD6292] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
             Registrar Producto
           </button>
         </div>
+        {showAlert && (
+          <div
+            className="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800 mt-4"
+            role="alert"
+          >
+            <svg
+              className="flex-shrink-0 inline w-4 h-4 me-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span className="sr-only">Info</span>
+            <div>
+              <span className="font-medium">Producto agregado con éxito!</span> Tu producto ha sido registrado.
+            </div>
+          </div>
+        )}
       </form>
     </div>
   )
