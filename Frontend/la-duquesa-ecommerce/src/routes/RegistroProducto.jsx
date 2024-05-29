ae461bb41333dbdb7f809715aed90fceba052320
SoyJeroMe <ojeronimo360@gmail.com>
2024-05-29 09:16:20 -0500

    cambiar color de alerta

--- Frontend/la-duquesa-ecommerce/src/routes/RegistroProducto.jsx
+++ Frontend/la-duquesa-ecommerce/src/routes/RegistroProducto.jsx
@@ -171,9 +171,9 @@ function RegistroProducto () {
             >
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
             </svg>
-            <span className="sr-only">Info</span>
-            <div>
-              <span className="font-medium">Producto agregado con éxito!</span>{' '}
+            <span className="sr-only text-w">Info</span>
+            <div className='text-white'>
+              <span className="font-medium text-[#BD6292]">Producto agregado con éxito!</span>{' '}
               Tu producto ha sido registrado.
             </div>
           </div>
