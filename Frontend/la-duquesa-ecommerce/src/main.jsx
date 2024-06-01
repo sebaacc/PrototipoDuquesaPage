b9a4a164a96aed0af634374caf8f7463a9d74c37
SoyJeroMe <ojeronimo360@gmail.com>
2024-06-01 17:15:59 -0500

    agregar form de inicio de sesion

--- Frontend/la-duquesa-ecommerce/src/main.jsx
+++ Frontend/la-duquesa-ecommerce/src/main.jsx
@@ -11,6 +11,7 @@ import CarritoDeCompras from './routes/CarritoDeCompras.jsx'
 import Tienda from './routes/Tienda.jsx'
 import ReporteDeUsuario from './routes/ReporteDeUsuario.jsx'
 import ReporteDeProducto from './routes/ReporteDeProducto.jsx'
+import LogIn from './routes/LogIn.jsx'
 
 const router = createBrowserRouter([
   {
@@ -51,6 +52,11 @@ const router = createBrowserRouter([
   {
     path: 'reporte-producto',
     element: <ReporteDeProducto />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: 'iniciar-sesion',
+    element: <LogIn />,
     errorElement: <ErrorPage />
   }
 ])
