7cde0508395ea0a7675e0b178dc5f7e8529ff5f0
SoyJeroMe <ojeronimo360@gmail.com>
2024-06-04 06:23:56 -0500

    corregir error en el formulario de login

--- Frontend/la-duquesa-ecommerce/src/main.jsx
+++ Frontend/la-duquesa-ecommerce/src/main.jsx
@@ -55,7 +55,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />
   },
   {
-    path: 'iniciar-sesion',
+    path: 'login',
     element: <LogIn />,
     errorElement: <ErrorPage />
   }
