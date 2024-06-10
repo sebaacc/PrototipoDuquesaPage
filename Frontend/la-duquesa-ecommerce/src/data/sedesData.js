8596d706e421a61caab4d6034b9ca775100c547b
SoyJeroMe <ojeronimo360@gmail.com>
2024-06-10 12:33:22 -0500

    agregar nuestras sedes

---
+++ Frontend/la-duquesa-ecommerce/src/data/sedesData.js
@@ -0,0 +1,33 @@
+import medellin from '../img/sedes/medellin.jpg'
+import bogota from '../img/sedes/Bogota.png'
+import cali from '../img/sedes/cali.png'
+import barranquilla from '../img/sedes/barranquilla.png'
+
+const sedes = [
+  {
+    imageSrc: bogota,
+    sedeTitle: 'Sede Bogotá',
+    sedeCityCountry: 'Bogotá, Colombia',
+    sedeDescription: 'Ubicada en el corazón de la capital, nuestra oficina en Bogotá ofrece una atención personalizada y profesional, con fácil acceso a los principales puntos de interés de la ciudad.'
+  },
+  {
+    imageSrc: medellin,
+    sedeTitle: 'Sede Medellín',
+    sedeCityCountry: 'Medellín, Colombia',
+    sedeDescription: 'Nuestra sede en Medellín está situada en una de las zonas más dinámicas de la ciudad, ofreciendo un ambiente moderno y acogedor para atender a nuestros clientes.'
+  },
+  {
+    imageSrc: cali,
+    sedeTitle: 'Sede Cali',
+    sedeCityCountry: 'Cali, Colombia',
+    sedeDescription: 'La oficina en Cali se destaca por su ubicación estratégica y sus instalaciones de última tecnología, brindando un servicio de excelencia en la Sucursal del Cielo.'
+  },
+  {
+    imageSrc: barranquilla,
+    sedeTitle: 'Sede Barranquilla',
+    sedeCityCountry: 'Barranquilla, Colombia',
+    sedeDescription: 'En la Puerta de Oro de Colombia, nuestra oficina de Barranquilla está lista para ofrecer soluciones eficientes y personalizadas en un entorno vibrante y acogedor.'
+  }
+]
+
+export default sedes
