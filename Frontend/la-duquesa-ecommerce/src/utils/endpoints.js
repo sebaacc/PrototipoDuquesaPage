d491d33c7e19f99a99b93d386d0fa8807b6570af
A-Javier-Fuentes <ale.isco.fuentess@gmail.com>
2024-06-21 19:18:19 -0300

    SaveUser

--- Frontend/la-duquesa-ecommerce/src/utils/endpoints.js
+++ Frontend/la-duquesa-ecommerce/src/utils/endpoints.js
@@ -2,6 +2,7 @@ const BASE_URL = 'http://localhost:8090'
 
 const endpoints = {
   getAndPostUser: `${BASE_URL}/users`, // se puede registrar un usuario u obtener los usuarios, cambiando entre post y get, y además cambiando la data/requested_Options que se le pasan
+  SaveUser: `${BASE_URL}/users/save`,
   postLogin: `${BASE_URL}/users/login`,
   postLogout: `${BASE_URL}/users/logout`,
   // getProducts: `${BASE_URL}/products`,
